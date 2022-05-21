05dabcaaf72de3695384efeb092228649c6ce9fb
Javier Pollak <javi.po.123@gmail.com>
2022-05-21 18:25:32 -0400

    Set useTabs to false in .prettierrc

--- svelte.config.js
+++ svelte.config.js
@@ -3,31 +3,31 @@ import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: [
-		preprocess({
-			scss: {
-				prependData: '@use "src/variables.scss" as *;'
-			},
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: [
+    preprocess({
+      scss: {
+        prependData: '@use "src/variables.scss" as *;'
+      },
 
-			postcss: true
-		})
-	],
+      postcss: true
+    })
+  ],
 
-	kit: {
-		adapter: adapter(),
+  kit: {
+    adapter: adapter(),
 
-		vite: {
-			css: {
-				preprocessorOptions: {
-					scss: {
-						additionalData: '@use "src/variables.scss" as *;'
-					}
-				}
-			}
-		}
-	}
+    vite: {
+      css: {
+        preprocessorOptions: {
+          scss: {
+            additionalData: '@use "src/variables.scss" as *;'
+          }
+        }
+      }
+    }
+  }
 };
 
 export default config;
