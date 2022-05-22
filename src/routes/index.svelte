06dfbb5c25d0a9f1e84725660aa5acc7816167e2
Javier Pollak <javi.po.123@gmail.com>
2022-05-21 23:24:54 -0400

    Implement basic ListingCard component

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,15 @@
 <script lang="ts">
   import Header from '../lib/components/Header.svelte';
-  import ListingCard from '$lib/ListingCard.svelte';
+  import ListingCard from '$lib/components/ListingCard.svelte';
+  import type { Pioneer } from '$lib/session';
+
+  const dummyPioneer: Pioneer = {
+    id: 'lkjkjlkad',
+    firstName: 'John',
+    lastName: 'Talon',
+    avatarUrl:
+      'https://cdn.discordapp.com/avatars/824318055504150579/b590c0b35c86f71ba1df02b7794bbe48.webp?size=160'
+  };
 
   const cards = [
     {
@@ -9,7 +18,8 @@
       mission:
         'We believe everyone Lorem ipsum dolor sit amet, consectetur adipiscing elit. Egestas sit turpis vel a.',
       image:
-        'https://c.gitcoin.co/grants/ac49910ab6c4693df8c68a71a98fe14b/0000001.png'
+        'https://c.gitcoin.co/grants/ac49910ab6c4693df8c68a71a98fe14b/0000001.png',
+      pioneer: dummyPioneer
     },
     {
       id: 2,
@@ -17,7 +27,8 @@
       mission:
         'We believe everyone Lorem ipsum dolor sit amet, consectetur adipiscing elit. Egestas sit turpis vel a.',
       image:
-        'https://c.gitcoin.co/grants/efa9d89ad3b641b2485536b6d4d710c3/130274396-5b016393-d6f2-4eba-9660-92a2983c8dd5.jpeg'
+        'https://c.gitcoin.co/grants/efa9d89ad3b641b2485536b6d4d710c3/130274396-5b016393-d6f2-4eba-9660-92a2983c8dd5.jpeg',
+      pioneer: dummyPioneer
     }
   ];
 </script>
