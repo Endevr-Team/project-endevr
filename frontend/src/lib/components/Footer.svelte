926f2d0027165b860261f17ee0511066b8daffbc
Jingxiang Mo <65676392+jingxiangmo@users.noreply.github.com>
2022-05-26 22:49:29 -0400

    New frontend: Create react app

--- frontend/src/lib/components/Footer.svelte
+++ frontend/src/lib/components/Footer.svelte
@@ -1,13 +1,15 @@
 <script>
+    let chainlink_src = 'frontend/static/chainlink-logo.png';
+    let filecoin_src = 'frontend/static/filecoin-logo.png';
+    let moralis_src = 'frontend/static/moralis-logo.png';
 </script>
 
 <footer>
     <p> Powered by: </p>
-
     <div class="div">
-    <img src="frontend/static/chainlink-logo.png" alt="chainlink logo" width="100px" height="100px"/>
-    <img src="frontend/static/filecoin-logo.png" alt="filecoin logo" width="100px" height="100px"/>
-    <img src="frontend/static/moralis-logo.png" alt="moralis logo" width="100px" height="100px"/>
+        <img src={chainlink_src} alt="chainlink logo" width="100px" height="100px"/>
+        <img src={filecoin_src} alt="filecoin logo" width="100px" height="100px"/>
+        <img src={moralis_src} alt="moralis logo" width="100px" height="100px"/>
     </div>
 </footer>
 
