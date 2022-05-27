fb598d01ccf34cd2c551fdbd98557543e646e812
Jingxiang Mo <65676392+jingxiangmo@users.noreply.github.com>
2022-05-26 22:51:57 -0400

    Added tailwindCSS

--- new-frontend/src/App.js
+++ new-frontend/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 
 function App() {
   return (
+
+
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -17,7 +19,16 @@ function App() {
         >
           Learn React
         </a>
+
+
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+
       </header>
+
+
+
     </div>
   );
 }
