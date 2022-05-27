7413dae657608749a45db4aae548486048da4a88
Jingxiang Mo <65676392+jingxiangmo@users.noreply.github.com>
2022-05-27 01:31:20 -0400

    Basic Project card completed

--- new-frontend/src/pages/HomePage.js
+++ new-frontend/src/pages/HomePage.js
@@ -1,9 +1,13 @@
 import NavBar from '../components/NavBar';
+import ProjectCard from '../components/ProjectCard';
 
 
 function HomePage() {
     return(
-        <NavBar/>
+        <div>
+            <NavBar/>
+            <ProjectCard/>
+        </div>
     )
 }
 
