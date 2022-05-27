68640f88bd38a4ba47987fa358240b04076afcb3
Jingxiang Mo <65676392+jingxiangmo@users.noreply.github.com>
2022-05-27 01:18:21 -0400

    Basic NavBar component completed

---
+++ new-frontend/src/components/NavBar.js
@@ -0,0 +1,12 @@
+
+function NavBar() {
+    return(
+        <div class="flex m-1">
+            <h1 class="text-3xl">Endevr</h1>
+            <button class="justify-end mx-2 px-5 bg-black text-white"> About </button>
+            <button class="justify-end mx-2 px-5 bg-red-500 text-white"> Join </button>
+        </div>
+    )
+}
+
+export default NavBar;
---
+++ new-frontend/src/pages/HomePage.js
@@ -0,0 +1,10 @@
+import NavBar from '../components/NavBar';
+
+
+function HomePage() {
+    return(
+        <NavBar/>
+    )
+}
+
+export default HomePage;
