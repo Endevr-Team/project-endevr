19ed2bf4c4e6c81c28abdb0d210b8eb1a4df7549
Jingxiang Mo <65676392+jingxiangmo@users.noreply.github.com>
2022-05-27 20:12:45 -0400

    Added progress bar

--- new-frontend/src/index.js
+++ new-frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import 'tw-elements';
 
 import reportWebVitals from './reportWebVitals';
 
--- new-frontend/src/pages/ProjectPage.js
+++ new-frontend/src/pages/ProjectPage.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
 import exampleImg from '../images/example.png'
+import ProgressBar from '../components/ProgressBar'
+
+
+const testData = [
+    { bgcolor: "#6a1b9a", completed: 60 },
+];
 
 function ProjectPage () {
     return (
@@ -14,43 +20,47 @@ function ProjectPage () {
                     <img className="object-fill" src={exampleImg} alt="Logo"/>
                 </div>
 
-                <h1 class="text-5xl font-bold text-slate-600 pb-2 mt-4"> Neural Rad</h1>
-                <p class="text-xl text-slate-600">
-                    Neural Rad is a web application that allows users to create and share neural networks.
-                    Users can then use the neural network to predict the outcome of a given set of inputs.
-                    Neural Rad is built using React, Express, Node, and PostgreSQL.
-                </p>
-
-                <h1 className="text-5xl font-bold text-slate-600 pb-2 mt-4"> Goals </h1>
-
-
-                <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
-                    <div className="bg-blue-600 h-2.5 rounded-full" style="width: 45%"></div>
+                <div>
+                    <h1 class="text-5xl font-bold text-slate-600 pb-2 mt-4"> Neural Rad</h1>
+                    <p class="text-xl text-slate-600">
+                        Neural Rad is a web application that allows users to create and share neural networks.
+                        Users can then use the neural network to predict the outcome of a given set of inputs.
+                        Neural Rad is built using React, Express, Node, and PostgreSQL.
+                    </p>
                 </div>
 
-                <p className="text-xl text-slate-600">
-                    Neural Rad is a web application that allows users to create and share neural networks.
-                    Users can then use the neural network to predict the outcome of a given set of inputs.
-                    Neural Rad is built using React, Express, Node, and PostgreSQL.
-                </p>
-
-                <h1 className="text-5xl font-bold text-slate-600 pb-2 mt-4"> Project Description </h1>
-                <p className="text-xl text-slate-600">
-                    Neural Rad is a web application that allows users to create and share neural networks.
-                    Users can then use the neural network to predict the outcome of a given set of inputs.
-                    Neural Rad is built using React, Express, Node, and PostgreSQL.
-                </p>
-
-                <h1 className="text-5xl font-bold text-slate-600 pb-2 mt-4"> Pioneers </h1>
-                <p className="text-xl text-slate-600">
-                    Neural Rad is a web application that allows users to create and share neural networks.
-                    Users can then use the neural network to predict the outcome of a given set of inputs.
-                    Neural Rad is built using React, Express, Node, and PostgreSQL.
-                </p>
+                <div>
 
 
+                    <h1 className="text-5xl font-bold text-slate-600 pb-2 mt-4"> Goal </h1>
+                    {testData.map((item, idx) => (
+                        <ProgressBar key={idx} bgcolor={item.bgcolor} completed={item.completed} />
+                    ))}
+                    <p className="text-xl text-slate-600">
+                        Neural Rad is a web application that allows users to create and share neural networks.
+                        Users can then use the neural network to predict the outcome of a given set of inputs.
+                        Neural Rad is built using React, Express, Node, and PostgreSQL.
+                    </p>
+                </div>
+
+                <div>
+                    <h1 className="text-5xl font-bold text-slate-600 pb-2 mt-4"> Project Description </h1>
+                    <p className="text-xl text-slate-600">
+                        Neural Rad is a web application that allows users to create and share neural networks.
+                        Users can then use the neural network to predict the outcome of a given set of inputs.
+                        Neural Rad is built using React, Express, Node, and PostgreSQL.
+                    </p>
+                </div>
 
 
+                <div>
+                    <h1 className="text-5xl font-bold text-slate-600 pb-2 mt-4"> Pioneers </h1>
+                    <p className="text-xl text-slate-600">
+                        Neural Rad is a web application that allows users to create and share neural networks.
+                        Users can then use the neural network to predict the outcome of a given set of inputs.
+                        Neural Rad is built using React, Express, Node, and PostgreSQL.
+                    </p>
+                </div>
             </div>
 
         </div>
