08f9f82f32b1d50b01032117a46c32566a76ff86
Anton Otaner <anton.otaner@elits.com>
2022-05-27 21:40:30 -0400

    Update ProjectPage.js

--- new-frontend/src/pages/ProjectPage.js
+++ new-frontend/src/pages/ProjectPage.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import exampleImg from '../images/example.png'
 import ProgressBar from '../components/ProgressBar'
+import { useLocation } from 'react-router-dom'
+import { useMoralisQuery } from "react-moralis";
+import { Link } from "react-router-dom";
 
 
 const testData = [
@@ -12,37 +15,61 @@ const testData = [
 
 
 function ProjectPage() {
+
+    const location = useLocation();
+    const id = location.pathname.replace('/', '');
+    const { data, error, isLoading } = useMoralisQuery("endeavours", (query) => query.equalTo("objectId", id));
+
     return (
 
         <div className="mx-10 mt-20 flex flex-col">
 
-            <div className="mx-5">
+            {isLoading ?
+                <h1 className="text-4xl font-semibold text-center">Loading</h1>
+                :
+                <>
+                    {
+                        data.length == 0 ?
 
-                <div>
-                    <h1 className="text-5xl font-bold text-gray-900 pb-2 mt-4 mb-8"> Neural Rad</h1>
-                    <p className="text-lg text-gray-800">
-                        Neural Rad is a web application that allows users to create and share neural networks.
-                        Users can then use the neural network to predict the outcome of a given set of inputs.
-                        Neural Rad is built using React, Express, Node, and PostgreSQL.
-                    </p>
-                </div>
+                            <>
+                                <h1 className="text-8xl font-semibold text-center">Project Not Found</h1>
+                                <Link to="/" className="mt-auto self-center">
+                                    <button className="mt-10 self-center h-8 px-4 bg-transparent text-black font-semibold border border-black rounded hover:bg-black hover:text-white hover:border-transparent transition ease-in duration-200 transform w-36"
+                                    > Go Home
+                                    </button>
+                                </Link>
+                            </>
 
-                <div className='mt-10'>
-                    <h1 className="text-4xl font-bold text-gray-900 pb-2 mt-4"> Goal </h1>
+                            :
 
-                    <div className="flex flex-row mb-10">
-                        {testData.map((item, idx) => (
-                            <ProgressBar key={idx} bgcolor={item.bgcolor} completed={item.completed} />
-                        ))}
-                    </div>
+                            <div className="mx-5">
 
-                    <button className="h-12 px-4 bg-transparent text-black font-semibold border border-black rounded hover:bg-black hover:text-white hover:border-transparent transition ease-in duration-200 transform w-58"
-                    > Become a contributor
-                    </button>
+                                <div>
+                                    <h1 className="text-5xl font-bold text-gray-900 pb-2 mt-4 mb-8">{data[0].get("name")}</h1>
+                                    <p className="text-lg text-gray-800">
+                                        {data[0].get("description")}
+                                    </p>
+                                </div>
 
-                </div>
+                                <div className='mt-10'>
+                                    <h1 className="text-4xl font-bold text-gray-900 pb-2 mt-4"> Goal </h1>
 
-            </div>
+                                    <div className="flex flex-row mb-10">
+                                        {testData.map((item, idx) => (
+                                            <ProgressBar key={idx} bgcolor={item.bgcolor} completed={item.completed} />
+                                        ))}
+                                    </div>
+
+                                    <button className="h-12 px-4 bg-transparent text-black font-semibold border border-black rounded hover:bg-black hover:text-white hover:border-transparent transition ease-in duration-200 transform w-58"
+                                    > Become a contributor
+                                    </button>
+
+                                </div>
+
+                            </div>
+                    }
+                </>
+            }
 
         </div>
 
